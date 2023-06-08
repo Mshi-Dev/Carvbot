2b369062a623a5b330a69fcee0de95c2c2bce022
Mshi-Dev <133026046+Mshi-Dev@users.noreply.github.com>
2023-06-09 00:26:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,4 +26,15 @@ function goToPage(pageId) {
     window.location.href = `${pageId}.html`;
 }
 
+//loading screen
+document.addEventListener("DOMContentLoaded", function () {
+    var loadingScreen = document.getElementById("loading-screen");
 
+    // Show loading screen initially
+    loadingScreen.style.display = "flex";
+
+    // Redirect to the main page after 6 seconds
+    setTimeout(function () {
+        window.location.href = "index.html";
+    }, 6000);
+});
